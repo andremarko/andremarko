f7767a1a4d4f1863e711da88a4f53e5dace5f58f
André Marcolongo <151890840+andremarko@users.noreply.github.com>
2024-06-10 15:47:45 -0300

    Create AboutMe.java

---
+++ AboutMe.java
@@ -0,0 +1,15 @@
+package people;
+
+public class AboutMe {
+  public static void main(String[] args){
+    
+    String name = "André;
+    String occupation = "Software Development Student";
+    String location = "São Paulo, SP - Brasil";
+
+    System.out.println("Name: " + name);
+    System.out.println("Occupation: " + occupation);
+    System.out.println("Location: " + location);
+  
+  }
+}
